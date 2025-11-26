56f5fe10cb4a9e05a12fd121801df5fad7db15b1
zpfhfh0124 <48612497+zpfhfh0124@users.noreply.github.com>
2025-11-26 22:14:26 +0900

    DirectX 11 개발 환경 세팅

---
+++ DX11Engine/DX11Engine/Main.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Windows.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow) 
+{
+	return 0;
+}
